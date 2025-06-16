65720a50d1486537b17800f5e9608f6ffb06befa
yatheeshv <yatheeshvv@gmail.com>
2025-06-16 17:55:17 +0530

    'sdf'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com/api'
+  apiUrl: 'git remote add origin https://github.com/Yatheesh-cmd/hotelbook.git'
 };
